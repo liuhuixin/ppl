9f085537e97563db224aed2459a3b88338ec55b2
liuhuixin <865043357@qq.com>
2021-09-27 23:21:41 +0800

    Update Public Static Page 2021年09月27日 周一 23:21:41

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 echo "begin build blog"
+
+# Commit changes.
+msg="Update Public Static Page `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+
 hugo
 git add .
-git commit -m "Update Hugo Blog"
+git commit -m "$msg"
 git push origin master
 
 # Go To Public folder
@@ -14,14 +22,7 @@ git remote add origin https://github.com/liuhuixin/ppl-s-Blog.git
 git pull
 # Add changes to git.
 git add .
-
-# Commit changes.
-msg="Update Public Static Page `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
 git commit -m "$msg"
-
 # Push source and build repos.
 git push origin master
 
